8871d0f050ff24b22c2c7ec18a2eaea9e6b061b9
Harry Hong <hong091114@gmail.com>
2023-06-24 23:23:29 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# evento-subgraph
+From Unlock Protocol Using [The Graph](https://thegraph.com).
+
+### Building the subgraph
+
+```sh
+# create correct ABIs and config files
+yarn prepare
+
+# generate graph code from source
+yarn codegen
+
+# build Web Assemly binaries
+yarn build <network-name>
+```
+
+### Config
+
+#### Multiple chains 
+
+Learning how Unlock team works.
+
+There is subgraph deployed for each network where Unlock Protocol is deployed. While code is similar for all, the addresses where the contracts are deployed vary, requiring a specifying config per network stored in `networks.json`.
+
+The `networks.json` file is generated from our `@unlock-protocol/networks` package.
+
+```sh
+# build the `subgraph.yaml` with the correct contract address per network
+yarn prepare:networks
+```
+
+### Deploying The Subgraph
+
+Deploy the latest subgraph code to the graph node.
+
+```sh
+export SUBGRAPH_DEPLOY_KEY=<api-key>
+
+# build
+yarn run build <network-name>
+
+# deploy a single network
+yarn run deploy <network-name>
+
+# deploy all networks
+yarn run deploy-all
+```
+
+## Tests
+
+(preferred way) Run the tests using Docker
+
+```
+yarn test -d
+```
+
+Run coverage
+
+```
+yarn test -d -c
+```
+
+## Helpers
+
+Show all events from different contract versions.
+
+```sh
+yarn show-events
+```
+
+## Add to the subgraph
+
+To add a particular property to the subgraph you need to
+
+1. add to schema
+2. edit the mappings
+3. test it
+4. republish the new subgraph
---
+++ bin/networks.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs-extra')
+const networksConfig = require('@unlock-protocol/networks')
+
+const networkFilePath = path.join(__dirname, '..', 'networks.json')
+
+// Some networks have a custom networkName
+const networkName = (n) => {
+  return networksConfig[n].subgraph.networkName || n
+}
+
+const generateNetworksFile = async () => {
+  const networks = Object.keys(networksConfig)
+    .filter((d) => !['networks', 'default', 'localhost'].includes(d))
+    .reduce((acc, chainName) => {
+      const {
+        startBlock,
+        unlockAddress: unlockContractAddress,
+        previousDeploys,
+      } = networksConfig[chainName]
+
+      const previous = {}
+      if (previousDeploys) {
+        previousDeploys.forEach(({ unlockAddress: address, startBlock }, i) => {
+          previous[`Unlock${i}`] = { address, startBlock }
+        })
+      }
+
+      const unlock = {
+        Unlock: {
+          address: unlockContractAddress,
+          startBlock,
+        },
+        ...previous,
+      }
+
+      return {
+        ...acc,
+        [networkName(chainName)]: unlock,
+      }
+    }, {})
+
+  fs.writeJSONSync(networkFilePath, networks, { spaces: 2 })
+  console.log(`Networks file saved at: ${networkFilePath}`)
+}
+
+module.exports = {
+  networkName,
+  generateNetworksFile,
+}
+
+// execute as standalone
+if (require.main === module) {
+  generateNetworksFile()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
+}
